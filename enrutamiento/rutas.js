c17703a53ff653449098eaa568b111031f0efb85
Leandro <ljbadillo7@soy.sena.edu.co>
2023-06-25 11:30:01 -0500

    se Realizo las cartas de los productos del carrito

--- enrutamiento/rutas.js
+++ enrutamiento/rutas.js
@@ -10,22 +10,24 @@ const router = express.Router();
 router.get('/formProducto',funciones.formProducto);
 router.get('/listaProductos',funciones.listaproductos);
 router.get('/catalogoProductos',funciones.catalogoProductos);
-router.post('/registrarProducto',funciones.nuevoProducto);
-router.post('/actualizarProducto',funciones.actualizarProducto);
 router.get('/descargarExcel',funciones.descargarExcel);
 router.get('/grafica',funciones.grafica);
 router.post('/cookiesProducto',funciones.cookiesProductos);
+router.post('/registrarProducto',funciones.nuevoProducto);
+router.post('/actualizarProducto',funciones.actualizarProducto);
+
 
 //usuarios
 router.get('/principal',funcionesUsuarios.paginaprincipal);
 router.get('/formUsuario',funcionesUsuarios.formUsuario);
-router.post('/registrarUsuario',funcionesUsuarios.nuevoUsuario);
-router.post('/autenticarUsuario',funcionesUsuarios.autenticarUsuario);
+router.get('/listaUsuarios',funcionesUsuarios.listaUsuarios);
 router.get('/inicioSesion',funcionesUsuarios.inicioSesion);
 router.get('/cookie',funcionesUsuarios.cookies);
 router.post('/enviarCorreo',funcionesUsuarios.enviarCorreo);
 router.post('/subirArchivo',funcionesUsuarios.subirArchivo);
-router.get('/listaUsuarios',funcionesUsuarios.listaUsuarios);
+router.post('/registrarUsuario',funcionesUsuarios.nuevoUsuario);
+router.post('/autenticarUsuario',funcionesUsuarios.autenticarUsuario);
+
 
 //vendedores
 router.get('/formVendedor',funcionesVendedores.formVendedores);
